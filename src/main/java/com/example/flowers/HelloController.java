b87a5db2043dfe52d386e94f88fa669a41e47939
Artemy <atrad96@mail.ru>
2023-08-15 17:04:59 +0300

    Flowers task

--- src/main/java/com/example/flowers/HelloController.java
+++ src/main/java/com/example/flowers/HelloController.java
@@ -1,11 +1,32 @@
 package com.example.flowers;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import java.text.DecimalFormat;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    Button btnMasha;
+    @FXML
+    Button btnTanya;
+    @FXML
+    Button btnDay;
+    @FXML
+    Image imgViolet;
+    @FXML
+    Image imgGeranium;
+    @FXML
+    Image imgCrocus;
+
 
     @FXML
     protected void onHelloButtonClick() {
